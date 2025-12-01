522fc882c555f01a45be50c54148eed3f4c030a2
Esdra <69021404+esdrasantos@users.noreply.github.com>
2025-12-01 04:40:42 +0000

    CHANGED:  created relationships between ORMs

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,16 @@
 from sqlalchemy import Column, String, Boolean, DateTime, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import uuid
 from datetime import datetime
 from .database import Base
+
+
+'''
+    1. relationship() is useful for returning nested json.
+    2. cascade="all, delete" ensures that milestones and rewards are deleted when the habit is deleted.
+    3. it allows the usage of habit.milestones, etc.
+'''
 
 class Habit(Base):
     __tablename__ = "habits"
@@ -13,6 +21,8 @@ class Habit(Base):
     is_completed = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+    milestones = relationship("Milestone", back_populates="habit", cascade="all, delete")
+
 class Milestone(Base):
     __tablename__ = "milestones"
 
@@ -20,6 +30,9 @@ class Milestone(Base):
     habit_id = Column(UUID(as_uuid=True), ForeignKey("habits.id"), nullable=False)
     target = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    habit = relationship("Habit", back_populates="milestones")
+    rewards = relationship("Reward", back_populates="milestones", cascade="all, delete")
 
 class Reward(Base):
     __tablename__ = "rewards"
@@ -29,3 +42,5 @@ class Reward(Base):
     title = Column(String, nullable=False)
     redeemed = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    milestone = relationship("Milestone", back_populates="rewards")
